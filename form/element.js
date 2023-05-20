bb7a7a91be028082e0790795bf08e66d8fd386ac
Anthony Muller <anthonyvmuller@outlook.com>
2023-05-20 14:59:31 -0300

    WIP

--- form/element.js
+++ form/element.js
@@ -1,13 +1,13 @@
-class AppHeader extends HTMLElement {
+class AppForm extends HTMLElement {
     async connectedCallback() {    
-        const cssFile = await fetch('./header/element.css');
+        const cssFile = await fetch('./form/element.css');
         const style = document.createElement('style');
         style.innerHTML = await cssFile.text()
         document.head.appendChild(style);
 
-        const htmlFile = await fetch('./header/element.html');
+        const htmlFile = await fetch('./form/element.html');
         this.innerHTML = await htmlFile.text();
     }
 }
 
-window.customElements.define('app-header', AppHeader)
+window.customElements.define('app-form', AppForm)
